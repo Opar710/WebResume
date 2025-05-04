faf053d229d67b1e7bfcc34cd4cf9991ef09a3a5
Opar710 <giannidaino@yahoo.com>
2025-05-04 17:22:27 -0500

    Navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,20 @@
+import React from 'react';
+
 export default function GMDNavbar() {
   return (
     <nav id="sideNav">
       <div className="sidebar-content">
         <h1 className="mobile-title">Mobile Web Specialist</h1>
-        <img className="profile-img" src="/assets/img/profile.jpg" alt="Gianni Massimo Daino" />
+        
+        {/* Image wrapper for red border */}
+        <div className="profile-img-wrapper">
+          <img
+            className="profile-img"
+            src="/assets/img/profile.jpg"
+            alt="Gianni Massimo Daino"
+          />
+        </div>
+
         <ul className="navbar-nav">
           <li><a className="nav-link" href="#about">About</a></li>
           <li><a className="nav-link" href="#experience">Experience</a></li>
